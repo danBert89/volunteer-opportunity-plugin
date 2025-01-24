ceb12a0e494889952f63765ba94b6d173d4fef51
unknown <dan_bertrand@live.ca>
2025-01-24 11:23:08 -0500

    Added activation function that creates new volunteer opportunity table in phpmyadmin

--- volunteer-opportunity-plugin.php
+++ volunteer-opportunity-plugin.php
@@ -1,8 +1,30 @@
 <?php
 
 /**
- * Plugin Name: Volunteer Oppotrunity Plugin
+ * Plugin Name: Volunteer Opportunity Plugin
  * Description: Assignment 01
  * Author: Daniel Bertrand
  * Version 1.0
  */
+
+// run this function on plugin activation to initialize database in phpmyadmin.
+function myplugin_activate()
+{
+    global $wpdb;
+
+    $wpdb->query("CREATE TABLE {$wpdb->prefix}volunteer_opportunity(
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                position VARCHAR(255),
+                organization VARCHAR(255),
+                type_of_commitment VARCHAR(255),
+                email VARCHAR(255),
+                description VARCHAR(255),
+                location VARCHAR(255),
+                hours INT,
+                skills_required TEXT);");
+}
+
+
+
+//Hooks
+register_activation_hook(__FILE__, 'myplugin_activate');
